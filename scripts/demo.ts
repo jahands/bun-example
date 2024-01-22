cf4532f25b7a1be9fde04a57239aca9aff8fc048
Jacob Hands <jacob@gogit.io>
2024-01-22 08:37:40 -0600

    Update demo.ts

--- scripts/demo.ts
+++ scripts/demo.ts
@@ -13,3 +13,8 @@ const queue = new pQueue({ concurrency: 2 })
 queue.add(() => $`sleep 1 && echo 1`)
 queue.add(() => $`sleep 1 && echo 2`)
 await queue.onIdle()
+
+for await (const line of $`date && sleep 1 && date && sleep 1`.lines()) {
+	console.log(line)
+}
+console.log(await $`date && sleep 1 && date && sleep 1`.text())
